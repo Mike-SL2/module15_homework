19c59cfd478a7f7ed8d787b08e4a23c4c232587c
Mike-SL2 <147996075+Mike-SL2@users.noreply.github.com>
2024-02-10 05:57:22 -0800

    Add files via upload

---
+++ 2/15_task_2.js
@@ -0,0 +1,13 @@
+﻿//задание 15.2 
+'use strict';
+
+const sizeButton = document.querySelector(".buttonPressMe");
+		
+sizeButton.addEventListener('click',()=>{const doc = document.documentElement;
+					 const screenA = " экрана: ";
+					 const pixels = " точек."; 
+					 let heightOfScreen = "Высота" + screenA + doc.clientHeight + pixels;
+					 let widthOfScreen =  "Ширина" + screenA + doc.clientWidth + pixels;
+alert(`${heightOfScreen} \n${widthOfScreen}`);
+});
+	

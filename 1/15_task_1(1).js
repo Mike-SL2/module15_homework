3f22463b9a51d9b4ab3c067a54a77b347194a38e
Mike-SL2 <147996075+Mike-SL2@users.noreply.github.com>
2024-02-10 05:56:22 -0800

    Add files via upload

---
+++ 1/15_task_1(1).js
@@ -0,0 +1,38 @@
+﻿//задание 15.1 вариант решения 1 - когда не загрузились стили
+
+"use strict";
+const taskNumber = "15.1";
+const variant = "1 (trigger)";
+const header = document.querySelector("h1");
+const buttonPressMe = document.querySelector(".buttonPressMe");
+const glyphWhite = document.querySelector(".buttonGlyphWhite");
+const glyphBlack = document.querySelector(".buttonGlyphBlack");
+
+header.innerHTML = `Задание ${taskNumber}, вариант решения ${variant}`;
+
+const changeIcons = (function () {
+  let blackGlyph;
+  const show = "block";
+  let whiteGlyph;
+  const hide = "none";
+
+  let trigger = false;
+  function changeIcons() {
+    trigger = !trigger;
+    if (trigger) {
+      whiteGlyph = show;
+      blackGlyph = hide;
+    } else {
+      whiteGlyph = hide;
+      blackGlyph = show;
+    }
+    glyphBlack.style.display = blackGlyph;
+    glyphWhite.style.display = whiteGlyph;
+  }
+  return changeIcons;
+})();
+
+buttonPressMe.addEventListener("click", changeIcons);
+
+changeIcons();
+
---
+++ 1/15_task_1(2).js
@@ -0,0 +1,16 @@
+﻿//задание 15.1 вариант решения 2
+
+"use strict";
+const taskNumber = "15.1";
+const variant = "2 (toggle)";
+const header = document.querySelector("h1");
+const buttonPressMe = document.querySelector(".buttonPressMe");
+const glyphWhite = document.querySelector(".buttonGlyphWhite");
+const glyphBlack = document.querySelector(".buttonGlyphBlack");
+
+header.innerHTML = `Задание ${taskNumber}, вариант решения ${variant}`;
+
+buttonPressMe.addEventListener("click", () => {
+  glyphWhite.classList.toggle("hideElement");
+  glyphBlack.classList.toggle("hideElement");
+});
